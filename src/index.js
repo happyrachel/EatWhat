1d2bbbf683f4699124788bc647d5a1e53e459610
Rachel Li <825255583@qq.com>
2017-03-06 11:38:44 +0800

    first

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-import EatWhat from './App.js';
-import './main.css'
-
-ReactDOM.render(
-  <EatWhat/>,document.getElementById('box')
-)
